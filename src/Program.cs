e9c0fac086677f9acfffd40cb25260d97eeb8418
shota <contact@shota.dev>
2022-07-13 12:58:40 +0200

    Add meta data

--- src/Program.cs
+++ src/Program.cs
@@ -8,16 +8,16 @@ namespace BSOD
     {
 
         [DllImport("ntdll.dll")]
-        public static extern IntPtr RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+        private static extern IntPtr RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
 
         [DllImport("ntdll.dll")]
-        public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+        private static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
 
         static void Main()
         {
-            bool previous;
+            bool previousValue;
             uint response;
-            RtlAdjustPrivilege(19, true, false, out previous);
+            RtlAdjustPrivilege(19, true, false, out previousValue);
             NtRaiseHardError(0xc000022A, 0, 0, IntPtr.Zero, 6, out response);
         }
 
